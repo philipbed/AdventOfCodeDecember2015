ddbba6f030c018cc6f6c93e6f12435ee51f822a1
Philip Bedward Jr <philipbed813@gmail.com>
2015-12-04 00:47:27 -0500

    Replaced with functional Python2.7 solution

--- Day 4/solution.py
+++ Day 4/solution.py
@@ -1,14 +1,34 @@
 import hashlib
+
+
 key = "iwrupvqb"
 
-success = "00000"
-for i in range(0,1000000):
-    copy = key
-    copy += str(i)
-    m = hashlib.md5()
-    m.update(copy)
-    h = m.hexdigest()
-    if h[:5] == success:
-        print(i)
-        break
+
+
+def part1():
+    part1Check= "00000"
+    oneMillion = 1000000
+    for i in range(100000,oneMillion):
+        copy = key
+        copy += str(i)
+        m = hashlib.md5()
+        m.update(copy)
+        h = m.hexdigest()
+        if h[:5] == part1Check:
+            print(i)
+            break
     
+def part2():
+    part2Check = "000000"
+    tenMillion = 10000000
+    for i in range(1000000,tenMillion):
+        copy = key
+        copy += str(i)
+        m = hashlib.md5()
+        m.update(copy)
+        h = m.hexdigest()
+        if h[:6] == part2Check:
+            print(i)
+            break
+part1()
+part2()
